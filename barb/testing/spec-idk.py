abe214fa9c591aeffcb724e25b150d1e690f114a
Morgan Waddy <mwaddy@bowser.phys.wvu.edu>
2020-09-25 15:35:41 -0400

    spec-idx functions

--- barb/testing/spec-idk.py
+++ barb/testing/spec-idk.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import emcee
 import argparse
 import sys
+import scipy as sp
 
 # cummulative rate of FRBs 
 def incd(f_inc, hunk):
@@ -20,11 +21,21 @@ global hunk
 # instantaneous rate 
 def obs(f_inc, radius, p):
     rs = np.power(radius/R, 2)
-    f_obs = f_inc * p
+    # p = ?
     # p is the function representing beam attenuation
+    f_obs = f_inc * p
     rat = np.power(2, rs) * f_obs
     r_obs = a * b * np.power(rat, -b-1) * np.power(2, rs) * 2 * np.pi * radius
     return r_obs
 
 # log likelihood
-def logger(
+def logger(flux, radius, p):
+    r_obs = obs(flux, radius, p)
+    # f1 = 
+    # integral1 = sp.integrate.quad(f1, )
+    # f2 = 
+    # Integral2 = sp.integrate.dblquad(f2, )
+    l1 = -tpb*(integral1) + np.sum(math.log(r_obs[, base]))
+    l2 = -tpb*(integral2) + np.sum(math.log(r_obs[, base]))
+    loggy = np.sum([l1, l2])
+    return loggy
