837a34065279c67d520aef6ee528bd2940571bad
Morgan Waddy <mdw4ux@virginia.edu>
2021-04-02 01:50:59 -0400

    changing structure

--- barb/barb_specidx.py
+++ barb/barb_specidx.py
@@ -25,10 +25,10 @@ from stats import sampling
 # sbatch -N1 -n19 --wrap="python barb/barb_specidx.py -f -D surveys/*.json -c 19 -s all_samples_MCMC"
 logging.basicConfig(filename="FRB-rate-calc.log", level=logging.INFO)
 parser = argparse.ArgumentParser(
-    description="""Bayesian Rate-Estimation for FRBs (BaRB)
+    description="Bayesian Rate-Estimation for FRBs (BaRB)
 sample command: python barb_specidx.py -f -D <name of the surveys> -c <number of cpus> -s <name of npz file>
 
-Surveys on the original data set: Agarwal 2019, Masui 2015, Men 2019, Bhandari 2018, Golpayegani 2019, Oslowski 2019, GREENBURST, Bhandari 2019, Qiu 2019, Shannon 2018, Madison 2019, Lorimer 2007, Deneva 2009, Keane 2010, Siemion 2011, Burgay 2012, Petroff 2014, Spitler 2014, Burke-Spolaor 2014, Ravi 2015, Petroff 2015, Law 2015, Champion 2016""",
+Surveys on the original data set: Agarwal 2019, Masui 2015, Men 2019, Bhandari 2018, Golpayegani 2019, Oslowski 2019, GREENBURST, Bhandari 2019, Qiu 2019, Shannon 2018, Madison 2019, Lorimer 2007, Deneva 2009, Keane 2010, Siemion 2011, Burgay 2012, Petroff 2014, Spitler 2014, Burke-Spolaor 2014, Ravi 2015, Petroff 2015, Law 2015, Champion 2016",
     formatter_class=argparse.RawTextHelpFormatter,
     )
 parser.add_argument(
