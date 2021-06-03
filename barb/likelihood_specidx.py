745c5e59c2e0e629a091f03ed5d478c11723cfde
Morgan Waddy <mdw4ux@virginia.edu>
2021-06-03 12:09:59 -0400

    updating structure

--- barb/likelihood_specidx.py
+++ barb/likelihood_specidx.py
@@ -3,51 +3,12 @@ import pylab as plt
 import matplotlib
 
 matplotlib.use("Agg")
-import math
 import sys
+import arg_stater
+import math
 import argparse
 import logging
 import emcee
-
-parser = argparse.ArgumentParser(
-    description="""Bayesian Rate-Estimation for FRBs (BaRB)
-sample command: python barb.py -f -D <name of the surveys> -c <number of cpus> -s <name of npz file>
-
-Surveys on the original data set: Agarwal 2019, Masui 2015, Men 2019, Bhandari 2018, Golpayegani 2019, Oslowski 2019, GREENBURST, Bhandari 2019, Qiu 2019, Shannon 2018, Madison 2019, Lorimer 2007, Deneva 2009, Keane 2010, Siemion 2011, Burgay 2012, Petroff 2014, Spitler 2014, Burke-Spolaor 2014, Ravi 2015, Petroff 2015, Law 2015, Champion 2016""",
-    formatter_class=argparse.RawTextHelpFormatter,
-)
-parser.add_argument(
-    "-D",
-    "--dat",
-    help="supply the input data after this flag",
-    action="store",
-    nargs="+",
-    required=True,
-)
-parser.add_argument(
-    "-c",
-    "--cpus",
-    help="supply the number of cpus you want to be used",
-    action="store",
-    nargs="+",
-    required=True,
-)
-parser.add_argument(
-    "-s",
-    "--allsamples",
-    help="supply the name of the output numpy array of the samples",
-    nargs=1,
-    action="store",
-    required=True,
-)
-parser.add_argument(
-    "-f",
-    "--freq",
-    action="store_true",
-    help="to estimate spectral index limits use this flag",
-    required=False,
-)
-args = parser.parse_args()
 
 def area(R, gamma):
     # beam shape
@@ -72,16 +33,16 @@ def freq_term(freq, freq_0, powterm):
     return freqqy
 
 
-def likelihood_list(data, alpha, beta):
+def likelihood_list(vargroup, alpha, beta):
     # runs through all data to return the likelihood that there will be
     # an FRB
     # a = Rref(freqqy**(alpha*gamma))
     if args.freq is True:
-        nFRBs, R, time, FWHM_2, flux, freq = data
+        nFRBs, R, time, FWHM_2, flux, freq = vargroup
         powterm = alpha * (beta - 1)
         freqqy = freq_term(freq, 1, powterm)
     else:
-        nFRBs, R, time, FWHM_2, flux = data
+        nFRBs, R, time, FWHM_2, flux = vargroup
     A = area(R, beta - 1)
     gamma = beta - 1
     Rref = alpha / gamma
@@ -109,6 +70,17 @@ def likelihood_list(data, alpha, beta):
                 )
         ll += val
     return ll
+
+def log_ll(varrest, vargroup):
+    alpha, beta = varrest
+    if args.freq is True:
+        nFRBs, R, time, FWHM_2, flux, freq = vargroup
+    else:
+        nFRBs, R, time, FWHM_2, flux = vargroup
+    alpha = 10 ** alpha
+    if beta < 1:
+        return -np.inf
+    return likelihood_list(vargroup, alpha=alpha, beta=beta)
 
 
 def cummlative_rate(flux, freq, Rref, gamma):
