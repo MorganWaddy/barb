9a0e17b1d1c5c8f4b242df00fd6a8b2c488618c5
Morgan Waddy <mdw4ux@virginia.edu>
2021-06-10 12:35:41 -0400

    new structure, no freq

--- barb/plotting.py
+++ barb/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import corner
 
 def check_likelihood(bb, lk, save = False):
     # in the mcmc you maximize the likelihood, this is to check if the functions are working
@@ -15,3 +16,21 @@ def make_hist(data, save = False, output_name = 'hist_MCMC'):
     if save == True:
         plt.savefig(output_name)
         plt.close(output_name)
+
+def make_corner(allsamples, save = False, figname = 'rates_mc.png'):
+    labels = [r"$\log \mathcal{R}$", r"$\alpha$"]
+
+    quantile_val = 0.99
+    
+    plt.figure(figsize=(15, 15))
+    corner.corner(
+        allsamples,
+        labels=labels,
+        quantiles=[(1 - 0.99) / 2, 0.5, 1 - (1 - 0.99) / 2],
+        show_titles=True,
+        bins=50,
+        )
+    if save == True:
+        # makes a corner plot displaying the projections of prob. distr. in space
+        plt.savefig(figname)
+        plt.close(figname)
