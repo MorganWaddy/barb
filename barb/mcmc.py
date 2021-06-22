b432c9f8b5d99424a5fd909c88b053a7a9192df4
Morgan Waddy <mdw4ux@virginia.edu>
2021-06-22 10:47:13 -0400

    updated

--- barb/mcmc.py
+++ barb/mcmc.py
@@ -8,20 +8,27 @@ import numpy as np
 import json
 import logging
 
-def sampling(p0, vargroup, cpu_num, nwalkers, ndim, filename = 'MCMC_results.h5', max_n = 100000):
+
+def sampling(
+    p0, vargroup, cpu_num, nwalkers, ndim, filename="MCMC_results.h5", 
+    max_n=100000
+    ):
     backend = emcee.backends.HDFBackend(filename)
     backend.reset(nwalkers, ndim)
-    ncpu = cpu_num
-    pool = Pool(ncpu)
-    # pool paralelizes the execution of the functions over the cpus
     nFRBs, R, time, FWHM_2, flux = vargroup
-   
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_ll, args = (vargroup), backend = backend, pool=pool)
+    ncpu = cpu_num
+    # pool paralelizes the execution of the functions over the cpus
+    pool = Pool(ncpu)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_ll, backend=backend, pool=pool)
+    max_n = max_n
+
     # tracking how the average autocorrelation time estimate changes
-    index=0
-    autocorr=np.empty(max_n)
+    index = 0
+    autocorr = np.empty(max_n)
+
     # variable for testing convergence
     old_tau = np.inf
+
     # sample for up to max_n steps
     for sample in sampler.sample(p0, iterations=max_n, progress=True):
         # only check convergence every 100 steps
@@ -29,40 +36,49 @@ def sampling(p0, vargroup, cpu_num, nwalkers, ndim, filename = 'MCMC_results.h5'
             continue
 
         # compute the autocorrelation time so far
-        tau=sampler.get_autocorr_time(tol=0)
-        autocorr[index]=np.mean(tau)
+        # using tol=0 means that we'll always get an estimate even
+        # if it isn't trustworthy
+        tau = sampler.get_autocorr_time(tol=0)
+        autocorr[index] = np.mean(tau)
         index += 1
 
         # check convergence
-        converged=np.all(tau * 100 < sampler.iteration)
+        converged = np.all(tau * 100 < sampler.iteration)
         converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
         if converged:
             break
-        old_tau=tau
-    return old_tau, sampler
+        old_tau = tau
+    return old_tau
+
+
+def read_samples(filename):
+    reader = emcee.backends.HDFBackend(filename)
+    tau = reader.get_autocorr_time()
+    # computes an estimate of the autocorrelation time for each parameter
+#    if np.isnan(tau).any():
+#        burnin = 100
+#        thin = 1
+#    else:
+    burnin = int(2 * np.max(tau))
+    # steps that should be discarded
+    thin = int(0.5 * np.min(tau))
+    samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
+    # gets the stored chain of MCMC samples
+
+    logging.info("burn-in: {0}".format(burnin))
+    logging.info("thin: {0}".format(thin))
+    logging.info("flat chain shape: {0}".format(samples.shape))
+    logging.info("samples: {0}".format(samples))
+    return samples
+
 
 def convert_params(samples):
     all_samples = samples
     # an array of the stored chain of MCMC samples
     all_samples[:, 0] = np.log10(
         (24 * 41253) * (10 ** all_samples[:, 0]) / (all_samples[:, 1] - 1)
-        )
+    )
     # 0 corresponds to R
     all_samples[:, 1] -= 1
     # 1 corresponds to alpha
     return all_samples
-
-def read_samples(filename):
-    reader = emcee.backends.HDFBackend(filename)
-    tau = reader.get_autocorr_time()
-    # computes an estimate of the autocorrelation time for each parameter
-    if np.isnan(tau).any():
-        burnin = 100
-        thin = 1
-    else:
-        burnin = int(2 * np.max(tau))
-        # steps that should be discarded
-        thin = int(0.5 * np.min(tau))
-    samples = reader.get_chain(discard=burnin, flat=True, thin=thin)
-    # gets the stored chain of MCMC samples
-    return samples
