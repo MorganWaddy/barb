366cbb84281ef6791f09b8fd50a348ea415842e6
Morgan Waddy <mdw4ux@virginia.edu>
2021-07-14 16:20:56 -0400

    added documentation

--- barb/likelihood.py
+++ barb/likelihood.py
@@ -2,10 +2,17 @@ import numpy as np
 
 
 def area(R, gamma):
-    # beam shape
-    # gamma is Euclidean scaling that is valid for any population with a fixed
-    # luminosity distribution, as long as the luminosity does not evolve with
-    # redshift and the population has a uniform spatial distribution
+    """
+    Calculate the beam shape
+
+    Args:
+        R (float): telescope radius
+        gamma (float): Euclidean scaling that is valid for any population with a fixed luminosity distribution, as long as the luminosity does not evolve with redshift and the population has a uniform spatial distribution
+
+    Returns:
+        ar (float): shape of the beam
+
+    """
     ar = (np.pi * R ** 2) / (gamma * np.log(2))
     beta = gamma + 1
     return ar
@@ -13,16 +20,39 @@ def area(R, gamma):
 
 def power_integral(FWHM_2, beta):
     # references the cummulative rate for observed fluxes > 0 (from paper)
-    # this returns the value for the integral of
-    # [sensitivity^(beta)]d(sensitivity)
+        """
+        Calculates the integral of [sensitivity^(beta)]d(sensitivity)
+
+        Args:
+            FWHM_2 (float): Full width at half-maximum divided by two (sensitivty, measured in janskys)
+            beta (float): Euclidean scaling (gamma +1)
+
+        Returns:
+            (FWHM_2 ** -(beta - 1)) / (beta - 1) (float)
+
+    """
     return (FWHM_2 ** -(beta - 1)) / (beta - 1)
-    # FWHM_2 is sigma (sensitivty), measured in janskys
 
 
 def likelihood_list(vargroup, alpha, beta):
-    # runs through all data to return the likelihood that there will be
-    # an FRB
-    # from appendix A: alpha = Rref*gamma
+        """
+        Analyzes all available data to return the likelihood that there will be an FRB
+
+        Args:
+            vargroup ([np.ndarray[float], np.ndarray[float], np.ndarray[float], np.ndarray[float], np.ndarray[float], np.ndarray[float]]): nFRBs, FWHM_2, R, beams, tpb, flux
+                nFRBs: number of FRBs detected
+                FWHM_2: full width at half-maximum divided by two
+                R: telescope radius 
+                beams: number of telescope beams
+                tpb: time per beam
+                flux: flux measurement of the FRB
+            alpha (float): product of reference rate and gamma
+            beta (float): Euclidean scaling (gamma +1)
+
+        Returns:
+            likelihood_list (np.ndarray[float]): list of likelihoods that there will be an FRB
+
+    """
     nFRBs, FWHM_2, R, beams, tpb, flux = vargroup
     A = area(R, beta - 1)
     I = power_integral(FWHM_2, beta)
@@ -50,6 +80,4 @@ def log_ll(varrest, nFRBs, FWHM_2, R, beams, tpb, flux):
         return -np.inf
         # returns a positive infinity multiplied by -1
         # (np.inf is a floating point constant value in the numpy library)
-#    if alpha < 0 or alpha > 5:
-#        return -np.inf
     return likelihood_list(vargroup, alpha=alpha, beta=beta)
