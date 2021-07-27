c634c8caa346432be6fe2ce1919b85eefdf058f5
Morgan W <42820730+MorganWaddy@users.noreply.github.com>
2021-07-27 17:28:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     license="",
     long_description=long_description,
     description="Bayesian Rate Estimation for FRBs",
-    install_requires=["astropy", "numpy", "matplotlib", "tqdm", "scipy", "emcee", "corner"],
+    install_requires=["astropy", "numpy", "matplotlib", "tqdm", "scipy", "emcee", "corner", "h5py"],
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python",
