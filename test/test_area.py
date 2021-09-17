77b538a987fb73a3ea5c389e4de9a4166a272844
Morgan Waddy <mdw4ux@virginia.edu>
2021-09-17 16:20:27 -0400

    area lest passed

--- test/test_area.py
+++ test/test_area.py
@@ -1,16 +1,8 @@
 import pytest
 import numpy as np
-
-
-def area(R, gamma):
-    # beam shape
-    # gamma is Euclidean scaling that is valid for any population with a fixed
-    # luminosity distribution, as long as the luminosity does not evolve with
-    # redshift and the population has a uniform spatial distribution
-    ar = (np.pi * R ** 2) / (gamma * np.log(2))
-    beta = gamma + 1
-    return ar
-
+import sys
+sys.path.insert(0, '../barb/')
+from likelihood import area
 
 def test_area():
     assert area(5, 7) == 16.187000506525692
