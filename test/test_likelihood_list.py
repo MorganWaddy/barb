d693c8e55afe0ee8513b80bd0585bcf620aa47d6
Morgan W <42820730+MorganWaddy@users.noreply.github.com>
2021-11-01 19:28:52 -0400

    Update test_likelihood_list.py

--- test/test_likelihood_list.py
+++ test/test_likelihood_list.py
@@ -4,7 +4,7 @@ from test_area import area
 from test_power_integral import power_integral
 import sys
 
-sys.path.insert(0, "../barb/")
+# sys.path.insert(0, "../barb/")
 from likelihood import likelihood_list
 
 vargroup = [4], 12, 5, 13, [6], [9]
