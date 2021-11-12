02e85a73363878a29f83f8b8375456c4bc3350ae
MorganWaddy <mdw4ux@virginia.edu>
2021-11-12 16:06:11 -0500

    black formatted

--- test/test_read_inputs.py
+++ test/test_read_inputs.py
@@ -4,16 +4,15 @@ import logging
 from barb.read_inputs import read_in
 
 jsons = ["Dummier_2020_Rate-Data.json", "Dummy_2020_Rate-Data.json"]
-# vargroup = (
-#    [2, 4],
-#    [0.24, 0.45],
-#    [0.1, 0.11666666666666667],
-#    [2, 3],
-#    [524, 568],
-#    [[4.2, 57.8, 10.2, 7.5], [7.2, 53.8, 9.2, 3.5]],
-# )
+vargroup = (
+    [2, 4],
+    [0.24, 0.45],
+    [0.1, 0.11666666666666667],
+    [2, 3],
+    [524, 568],
+    [[4.2, 57.8, 10.2, 7.5], [7.2, 53.8, 9.2, 3.5]],
+    )
 
 
 def test_read_in():
-    nFRBs, sensitivity, R, beams, tpb, flux = vargroup
     assert read_in(jsons) == vargroup
