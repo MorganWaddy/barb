1f4f84e60b9a7d6694da459ff0bd2f1c15a34cb3
Morgan W <42820730+MorganWaddy@users.noreply.github.com>
2021-11-12 15:19:20 -0500

    Update test_read_inputs.py

--- test/test_read_inputs.py
+++ test/test_read_inputs.py
@@ -6,15 +6,16 @@ import sys
 # sys.path.insert(0, "../barb/")
 from barb.read_inputs import read_in
 
-files = ["Dummier_2020_Rate-Data.json", "Dummy_2020_Rate-Data.json"]
-vargroup = (
-    [2, 4],
-    [0.24, 0.45],
-    [0.1, 0.11666666666666667],
-    [2, 3],
-    [524, 568],
-    [[4.2, 57.8, 10.2, 7.5], [7.2, 53.8, 9.2, 3.5]],
-)
+jsons = ["Dummier_2020_Rate-Data.json", "Dummy_2020_Rate-Data.json"]
+# vargroup = (
+#    [2, 4],
+#    [0.24, 0.45],
+#    [0.1, 0.11666666666666667],
+#    [2, 3],
+#    [524, 568],
+#    [[4.2, 57.8, 10.2, 7.5], [7.2, 53.8, 9.2, 3.5]],
+#)
+vargroup = nFRBs, sensitivity, R, beams, tpb, flux
 
 
 def test_read_in():
