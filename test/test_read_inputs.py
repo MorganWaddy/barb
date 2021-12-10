6759d13abc6654a488a61192a707ddef8b5726a2
MorganWaddy <mdw4ux@virginia.edu>
2021-12-09 21:30:14 -0500

    test accepted

--- test/test_read_inputs.py
+++ test/test_read_inputs.py
@@ -3,19 +3,21 @@ import json
 import logging
 from barb.read_inputs import read_in
 
-jsons = [
-    "/home/mwaddy/FRB-rate/barb/test/Dummier_2020_Rate-Data.json",
-    "/home/mwaddy/FRB-rate/barb/test/Dummy_2020_Rate-Data.json",
+files = [
+    "Dummier_2020_Rate-Data.json",
+    "Dummy_2020_Rate-Data.json",
 ]
-vargroup = (
+vargroup = [
     [2, 4],
     [0.24, 0.45],
     [0.1, 0.11666666666666667],
     [2, 3],
     [524, 568],
     [[4.2, 57.8, 10.2, 7.5], [7.2, 53.8, 9.2, 3.5]],
-)
+]
+vargroup = np.array(vargroup)
 
 
 def test_read_in():
-    assert read_in(jsons) == np.all(vargroup)
+    jsons = np.array(read_in(files))
+    assert jsons.all() == vargroup.all()
