b0cd5bffcac4dcf9f5d94a3af7f60ea017610236
MorganWaddy <mdw4ux@virginia.edu>
2021-12-31 12:18:36 -0500

    working on index error

--- test/test_log_ll.py
+++ test/test_log_ll.py
@@ -2,16 +2,15 @@ import pytest
 import numpy as np
 import sys
 
-# sys.path.insert(0, "../barb/")
 from barb.likelihood import area
 from barb.likelihood import power_integral
 from barb.likelihood import likelihood_list
 from barb.likelihood import log_ll
 
-vargroup = [4], 12, 5, 13, [6], [9]
-
+vargroup = [4], 12, 5, 13, np.array([6]), [9]
+vargroup = np.array(vargroup)
 
 def test_log_likelihood_list():
     idx = 0
-    nburst = 1
-    assert log_ll([10 ** 3, 7], [4], 12, 5, 13, [6], [9]) == -1.3369332333190052
+    nburst = 0
+    assert log_ll([3, 7], [4], 12, 5, 13, np.array([6]), [9]) == -1.3369332333190052
