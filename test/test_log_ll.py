aa85c7ec9c2df991801b4cfc3af594ec91b88109
MorganWaddy <mdw4ux@virginia.edu>
2022-01-13 21:31:49 -0500

    reformatted

--- test/test_log_ll.py
+++ test/test_log_ll.py
@@ -85,4 +85,4 @@ def log_ll(varrest, nFRBs, sensitivity, R, beams, tpb, flux):
 def test_log_likelihood_list():
     idx = 0
     nburst = 1
-    assert log_ll([3, 7], [4], 12, 5, 13, np.array([6,6,6,6]), [9,9,9,9]) == -1.3369332333190052
+    assert log_ll([3, 7], [4], 12, 5, 13, np.array([6,6,6,6]), [9,9,9,9]) == 41.348502624892795
