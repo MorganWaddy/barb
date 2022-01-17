c506f3c0a9734a7ee94b16fa34156b4ce7bad4c0
MorganWaddy <mdw4ux@virginia.edu>
2022-01-17 13:49:18 -0500

    sampling test

---
+++ test/test_mcmc.py
@@ -0,0 +1,37 @@
+import pytest
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import emcee
+from tqdm import tqdm
+import numpy as np
+import json
+import logging
+
+from barb.likelihood import area
+from barb.likelihood import power_integral
+from test_likelihood import likelihood
+from test_likelihood import log_ll
+from barb.mcmc import sampling
+
+vargroup = (
+    [4],
+    12,
+    5,
+    13,
+    np.array([6, 6, 6, 6]),
+    [9, 9, 9, 9],
+)
+vargroup = np.array(vargroup, dtype=object)
+cpu_num=1
+ndim, nwalkers = 2, 12
+ivar = np.array([np.log10(15), 2.5])
+p0 = ivar + 0.05 * np.random.uniform(size=(nwalkers, ndim))
+filename="test_MCMC_results.h5"
+max_n=10
+
+def test_sampling():
+    old_tau = sampling(
+        p0, varrgroup, cpu_num, nwalkers, ndim, filename=h5name, max_n=max_n
+    )
+    assert old_tau
+
